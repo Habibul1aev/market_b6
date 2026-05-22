eb8d2939cc2518edf7ce941f38c6cc48b5d75592
Habibul1aev <hhabibullaevfirdavs@gmail.com>
2026-05-22 18:14:03 +0600

    allow host

--- project/settings.py
+++ project/settings.py
@@ -126,11 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'static_dirs'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static', 'static_dirs'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
